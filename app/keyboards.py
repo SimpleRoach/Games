d3c2442f86468009397564f6448639f13c4b04e3
Ramil <tlsmscience@gmail.com>
2024-04-24 22:45:06 +0500

    Обновление

--- app/keyboards.py
+++ app/keyboards.py
@@ -3,5 +3,5 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Крестики - Нолики')],
                                        [KeyboardButton(text='Камень - Ножницы - Бумага')],
-                                       [KeyboardButton(text='Статистика')],
-                                       [KeyboardButton(text='Стоп')]])
+                                       [KeyboardButton(text='Статистика'),
+                                       KeyboardButton(text='Стоп')]])
