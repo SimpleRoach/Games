71ba29ed1602eedf9210ef2b3282320e3a38f505
Ramil <tlsmscience@gmail.com>
2024-04-24 21:43:00 +0500

    ///

--- base/Base_bot.py
+++ base/Base_bot.py
@@ -1,15 +1,19 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 import asyncio
 from aiogram.types import Message
-
+from aiogram.filters import CommandStart, Command
 
 bot = Bot(token='6757514713:AAFbwHmv4BZMHHtqkf9WBA2YsDNEUbsHHgc')
 dp = Dispatcher()
 
-@dp.message()
+@dp.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer('Старт')
     await message.reply('Отвечаю')
+
+@dp.message(Command('help'))
+async def cmd_help(message: Message):
+    await message.answer('Нажато help')
 
 
 
@@ -18,7 +22,9 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    asyncio.run(main())
-
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот выключен')
 
 # executor.start_polling(dp, skip_updates=True)
