9e83cdb1005e35d31b17e5b1c3cae995664b8eeb
Ramil <tlsmscience@gmail.com>
2024-04-24 22:46:40 +0500

    Обновление

--- app/keyboards.py
+++ app/keyboards.py
@@ -4,4 +4,5 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Крестики - Нолики')],
                                        [KeyboardButton(text='Камень - Ножницы - Бумага')],
                                        [KeyboardButton(text='Статистика'),
-                                       KeyboardButton(text='Стоп')]])
+                                       KeyboardButton(text='Стоп')]],
+                           resize_keyboard=True,input_field_placeholder='Выбери команду')
